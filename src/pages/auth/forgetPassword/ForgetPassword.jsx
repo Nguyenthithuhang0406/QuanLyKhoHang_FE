224a3137438c475f18d61085c4454f60b403b35c
Bàng Nga <bangnga82@gmail.com>
2024-10-06 20:15:14 +0700

    man hinh quen mat khau

--- src/pages/auth/forgetPassword/ForgetPassword.jsx
+++ src/pages/auth/forgetPassword/ForgetPassword.jsx
@@ -2,9 +2,29 @@
 import React from 'react'
 
 import './ForgetPassword.css';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+
 const ForgetPassword = () => {
   return (
-    <div>ForgetPassword</div>
+    <Formik>
+      <Form className='s1'> 
+       <p htmlFor="email" className='s2'> <b>QUÊN MẬT KHẨU</b></p>
+        <div className='s3'>
+          <label htmlFor="email">Email</label>
+          <Field className='boder' name="email" type="email" />
+          <ErrorMessage name="email" />
+        </div>
+        <div className='s4'>
+          <label htmlFor="userName">Tên đăng nhập</label>
+          <Field className='boder' name="userName" type="text" />
+          <ErrorMessage name="userName" />
+        </div>
+        <div className='s5'>
+          <button className='button' type='submit'>Gửi lại mật khẩu</button>
+        </div>
+          
+      </Form>
+    </Formik>
   )
 }
 
