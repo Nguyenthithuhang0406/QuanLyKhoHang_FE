2c56660b3a6ef081f36d4b7383377c173dc73b0c
Trần Văn Huynh <106756683+huynh480@users.noreply.github.com>
2024-10-09 23:32:10 +0700

    man hinh xac thuc otp

--- src/pages/auth/confirmOTP/ConfirmOTP.jsx
+++ src/pages/auth/confirmOTP/ConfirmOTP.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable */
 import React from 'react'
 
-import './ConfirmOTP.css';
+import './Confirm.css';
+import Header from '../../../components/header/Header';
+
 const ConfirmOTP = () => {
   return (
-    <div>ConfirmOTP</div>
+    <>
+    <Header/>
+    <div className="otp-container">
+      <img src="src/assets/images/images.png" alt="Email Icon" />
+      <h2>Mã OTP gồm 6 chữ số đã được gửi đến bạn qua email</h2>
+      <input type="text" maxLength="6" placeholder="######" />
+      <p>Gửi lại OTP</p>
+      <button>Tiếp</button> 
+  
+    </div>
+
+    </>
   )
 }
 
