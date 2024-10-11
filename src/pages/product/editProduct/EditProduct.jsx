5989a2e65c42d705a9760908a5d72be354d66396
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-11 18:10:31 +0700

    ✨ them base du an

---
+++ src/pages/product/editProduct/EditProduct.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable*/
+import React from 'react'
+
+import './EditProduct.css';
+
+const EditProduct = () => {
+  return (
+    <div>EditProduct</div>
+  )
+}
+
+export default EditProduct;
