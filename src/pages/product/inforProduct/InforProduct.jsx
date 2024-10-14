6d38f5a854be6085f45ead4dc865b4f0e7a25656
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-14 21:30:46 +0700

    update

---
+++ src/pages/product/inforProduct/InforProduct.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react'
+
+import './InforProduct.css';
+
+const InforProduct = () => {
+  return (
+    <div>InforProduct</div>
+  )
+}
+
+export default InforProduct;
