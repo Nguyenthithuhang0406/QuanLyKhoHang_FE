84bd1e1d73d3cc44bdba16fde2f7ada677e606de
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-17 14:04:13 +0700

    add base sprint 3

---
+++ src/pages/importSlip/editImportSlip/EditImportSlip.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from 'react'
+
+import './EditImportSlip.css';
+const EditImportSlip = () => {
+  return (
+    <div>EditImportSlip</div>
+  )
+}
+
+export default EditImportSlip;
