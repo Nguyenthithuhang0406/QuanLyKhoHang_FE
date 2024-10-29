292b2ac32d19c12bdcde25a4f74e25a306d642c2
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-10-29 22:06:44 +0700

    api importSlip

---
+++ src/api/importSlipApi/importSlip.js
@@ -0,0 +1,66 @@
+import { authInstance, requestWithToken } from "@/utils/axios-http";
+
+export const createdImportSlip = async (data) => {
+  const { importSlipCode, providerId, userId, status, products, newProducts, contracts, type, reason } = data;
+  
+  await requestWithToken(authInstance,{
+    url: "/importSlip/createImportSlip",
+    method: "post",
+    data: {
+      importSlipCode,
+      providerId,
+      userId,
+      status,
+      products,
+      newProducts,
+      contracts,
+      type,
+      reason,
+    }
+  });
+
+};
+
+export const getImportSlipById = async (importSlipId) => {
+  const res = await requestWithToken(authInstance, {
+    url: `/importSlip/${importSlipId}`,
+    method: "get",
+  });
+
+  return res.data.data;
+};
+
+export const deletedImportSlip = async (importSlipId) => {
+  await requestWithToken(authInstance, {
+    url: `/importSlip/${importSlipId}`,
+    method: "delete",
+  });
+};
+
+export const updateStatusImportSlip = async (importSlipId, status) => {
+  await requestWithToken(authInstance, {
+    url: `/importSlip/${importSlipId}`,
+    method: "put",
+    data: {
+      status,
+    },
+  });
+};
+
+export const getImportSlipByType = async (type, page, limit) => {
+  const res = await requestWithToken(authInstance, {
+    url: `/importSlip/${type}?page=${page}&limit=${limit}`,
+    method: "get",
+  });
+
+  return res.data.data;
+}
+
+export const searchImportSlip = async (importSlipCode="", providerId="", agencyId="",customerId="", status="", timeStart, timeEnd, page, limit) => {
+  const res = await requestWithToken(authInstance, {
+    url: `/importSlip/searchImportSlips?page=${page}&limit=${limit}&importSlipCode=${importSlipCode}&providerId=${providerId}&agencyId=${agencyId}&customerId=${customerId}&status=${status}&timeStart=${timeStart}&timeEnd=${timeEnd}`,
+    method: "post",
+  });
+
+  return res.data.data;
+};
