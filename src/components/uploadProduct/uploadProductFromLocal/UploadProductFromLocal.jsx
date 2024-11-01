f690f6c7d5b875ed4f5156e1123f095797200d86
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-11-01 16:59:04 +0700

    lấy danh sách hàng hoá để chọn

--- src/components/uploadProduct/uploadProductFromLocal/UploadProductFromLocal.jsx
+++ src/components/uploadProduct/uploadProductFromLocal/UploadProductFromLocal.jsx
@@ -2,13 +2,16 @@
 import React, { useEffect, useState } from 'react'
 
 import './UploadProductFromLocal.css';
-import { getProducts } from '@/api/productApi/product';
+import { getProducts, searchProduct } from '@/api/productApi/product';
 import { Pagination } from 'antd';
-const UploadProductFromLocal = () => {
+const UploadProductFromLocal = ({ onCancel, selectedProducts, setSelectedProducts, isRefresh, setIsRefresh }) => {
   const [listProduct, setListPoroduct] = useState([]);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
   const [total, setTotal] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
+  const [productName, setProductName] = useState('');
+  const [productCode, setProductCode] = useState('');
 
   useEffect(() => {
     const getListProduct = async () => {
@@ -16,12 +19,73 @@ const UploadProductFromLocal = () => {
       setTotal(res.totalResult);
       setListPoroduct(res.products);
 
-      console.log(res.products);
+      const all = await getProducts(1, res.totalResult);
+      setAllProducts(all.products);
     };
 
     getListProduct();
   }, [page]);
 
+  const handleChangeCheckBox = (product) => {
+    setSelectedProducts((prev) => {
+      if (prev.some((item) => item._id === product._id)) {
+        return prev.filter((item) => item._id !== product._id);
+      }
+      else {
+        return [...prev, product];
+      }
+    });
+  };
+
+  const handleClickAll = async () => {
+    if (selectedProducts.length === total) {
+      setSelectedProducts([]);
+    } else {
+      setSelectedProducts(allProducts);
+    }
+  };
+
+  const handleClickAdd = () => {
+    setIsRefresh(!isRefresh);
+    onCancel();
+  };
+
+  const handleClickCancel = () => {
+    setSelectedProducts([]);
+    setIsRefresh(!isRefresh);
+    onCancel();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'productName') {
+      setProductName(value);
+    } else {
+      setProductCode(value);
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+      setProductCode('');
+      setProductName('');
+    }
+  };
+
+  const handleSearch = async () => {
+    try {
+      const res = await searchProduct(productCode, productName, page, limit);
+      console.log('res', res);
+      setListPoroduct(res.products);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+  };
   return (
     <div className='container_uplocal'>
       <div className='h1_uplocal'>
@@ -32,20 +96,20 @@ const UploadProductFromLocal = () => {
           <div className='inputInforuplocal'>
             <div className='boxuplocal'>
               <span className='nameuplocal'>Tên hàng hoá</span>
-              <input type="text" className='iNameuplocal' />
+              <input type="text" className='iNameuplocal' name='productName' value={productName} onChange={handleChange} onKeyDown={handleKeyDown} />
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
             <div className='boxuplocal'>
               <span className='IDuplocal'>Mã hàng hoá</span>
-              <input type="text" className='iIDuplocal' />
+              <input type="text" className='iIDuplocal' name='productCode' value={productCode} onChange={handleChange} onKeyDown={handleKeyDown} />
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
           </div>
         </div>
         <div className='listTableuplocal' >
           <div className='lb_up'>
-            <label htmlFor="" className='lb_upload'>0 mặt hàng được chọn</label>
-            <input className='box_chose' type="checkbox" name="" id="" />
+            <label htmlFor="" className='lb_upload'>{selectedProducts.length} mặt hàng được chọn</label>
+            <input className='box_chose' type="checkbox" name="" id="" onChange={handleClickAll} />
           </div>
           <table className='Listuplocal'>
             <tbody>
@@ -66,9 +130,14 @@ const UploadProductFromLocal = () => {
                     <td>{product.productCode}</td>
                     <td>{product.productDVT}</td>
                     <td>{product.productQuantityRemaining}</td>
-                    <td>{product.productPrice}</td>
+                    <td>{formatCurrency(product.productPrice)}</td>
                     <td className='choseuplocal'>
-                      <input className='box_chose' type="checkbox" name="" id="" />
+                      <input
+                        className='box_chose'
+                        type="checkbox" name="" id=""
+                        checked={selectedProducts.some((item) => item._id === product._id)}
+                        onChange={() => handleChangeCheckBox(product)}
+                      />
                     </td>
                   </tr>
                 ))
@@ -87,8 +156,8 @@ const UploadProductFromLocal = () => {
           </div>
         </div>
         <div className='b_uplocatol'>
-          <button className='b1_uplocatol' type='button'>Hủy</button>
-          <button className='b2_uplocatol' type="submit">Thêm</button>
+          <button className='b1_uplocatol' type='button' onClick={handleClickCancel}>Hủy</button>
+          <button className='b2_uplocatol' type="submit" onClick={handleClickAdd}>Thêm</button>
         </div>
       </div>
     </div >
