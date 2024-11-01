0d9bc1d02673ea17515f7d9b2282d568bead5dbf
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-11-01 16:59:24 +0700

    ghép API tạo phiếu nhập kho (chưa fix)

--- src/pages/importSlip/createdImportSlip/CreatedImportSlip.jsx
+++ src/pages/importSlip/createdImportSlip/CreatedImportSlip.jsx
@@ -1,14 +1,45 @@
 /* eslint-disable */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { useSelector } from 'react-redux';
 
 import Header from '@/components/header/Header'
 import NavBar from '@/components/navBar/NavBar'
-import './CreateImportSlip.css';
 import UploadProductFromLocal from '@/components/uploadProduct/uploadProductFromLocal/UploadProductFromLocal';
+import { searchSupply } from '@/api/suppliesAPI/supply';
+
+import './CreateImportSlip.css';
 const CreatedImportSlip = () => {
   const [showUploadFromLocal, setShowUploadFromLocal] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [fileNames, setFileNames] = useState([]);
+  const [listProvider, setListProvider] = useState([]);
+  const [providerInfor, setProviderInfor] = useState({
+    providerCode: "",
+    providerPhone: "",
+    providerAddress: "",
+  });
+  const [isRefresh, setIsRefresh] = useState(false);
+
+  const user = useSelector((state) => state.user);
+  const [newImportSlip, setNewImportSlip] = useState({
+    importSlipCode: `PNK${Math.floor(Math.random() * 1000000)}`,
+    providerId: "",
+    userId: user._id,
+    status: "PENDING",
+    products: [],
+    newProducts: [],
+    contracts: "",
+    type: "Provider",
+    reason: "",
+    importPrice: "0",
+  });
+
+  const [contract, setContract] = useState({
+    contractContent: "",
+    contractMedia: [],
+  });
 
   const navigate = useNavigate();
 
@@ -16,6 +47,115 @@ const CreatedImportSlip = () => {
     setShowUploadFromLocal(false);
   };
 
+  useEffect(() => {
+    const getProvider = async () => {
+      const res = await searchSupply("", "", "", "provider", 1, 100);
+      setListProvider(res.supplies);
+    };
+
+    getProvider();
+  }, []);
+
+  useEffect(() => {
+    setNewImportSlip({
+      ...newImportSlip,
+      products: selectedProducts.map((product) => ({ productId: product._id, quantity: 0, discount: 0 })),
+    });
+  }, [isRefresh]);
+
+  const handleChangeProvider = (e) => {
+    const { name, value } = e.target;
+    setNewImportSlip({ ...newImportSlip, [name]: value });
+
+    const provider = listProvider.find((p) => p._id === value);
+    if (provider) {
+      setProviderInfor({
+        providerCode: provider.providerCode,
+        providerPhone: provider.providerPhone,
+        providerAddress: provider.providerAddress,
+      });
+    }
+  };
+
+  const handleChangeQuantity = (e, productId) => {
+    const newQuantity = parseInt(e.target.value) || 0;
+    setNewImportSlip(prev => ({
+      ...prev,
+      products: prev.products.map((p) => {
+        if (p.productId === productId) {
+          return { ...p, quantity: newQuantity }
+        }
+        return p;
+      })
+    }))
+  };
+
+  const handleChangeDiscount = (e, productId) => {
+    const discountValue = e.target.value.replace('%', '');
+    const newDiscount = parseInt(discountValue) || 0;
+    setNewImportSlip(prev => ({
+      ...prev,
+      products: prev.products.map((p) => {
+        if (p.productId === productId) {
+          return { ...p, discount: newDiscount }
+        }
+        return p;
+      })
+    }))
+  };
+
+  const calculateLineTotal = (product) => {
+    const item = newImportSlip.products.find((p) => p.productId === product._id);
+    if (item) {
+      return +product.productPrice * item.quantity * (1 - item.discount / 100);
+
+    } else {
+      return 0;
+    }
+  };
+
+  const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    if (newImportSlip.products.length > 0) {
+      newImportSlip.products.forEach((product) => {
+        const productPrice = selectedProducts.find((p) => p._id === product.productId)?.productPrice;
+        totalPrice += +productPrice * product.quantity * (1 - product.discount / 100);
+      });
+    }
+    setNewImportSlip(prev => ({
+      ...prev,
+      importPrice: `${totalPrice}`
+    }));
+    return totalPrice;
+  };
+
+  const formatCurrency = (value) => {
+    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = Array.from(e.target.files);
+    const newRawFile = [...contract.contractMedia, ...selectedFile];
+
+    // for (const file of selectedFile) {
+    //   newRawFile.push(file);
+    // };
+    setContract({ ...contract, contractMedia: newRawFile });
+    setFileNames([...fileNames, ...selectedFile.map((file) => file.name)]);
+  };
+
+  const handleChangeFileNameContract = (e) => {
+    const updateFileNames = e.target.value.split(', ').map((name) => name);
+    const removeFiles = fileNames.filter((name) => !updateFileNames.includes(name));
+
+    const updateContractMedia = contract.contractMedia.filter((file) => !removeFiles.includes(file.name));
+    setContract({ ...contract, contractMedia: updateContractMedia });
+  };
+
+  const handleSubmit = async () => {
+    // console.log("contract", contract);
+    console.log("newImportSlip", newImportSlip);
+  };
   return (
     <div>
       <Header className='headerListP' />
@@ -33,94 +173,139 @@ const CreatedImportSlip = () => {
             <div className='i-line1'>
               <div className='i-o'>
                 <div className='i-name'>Nguồn xuất</div>
-                <select name="source" id="source">
+                <select name="providerId" id="source" onChange={handleChangeProvider}>
+                  <option value=""> -Chọn nguồn- </option>
+                  {
+                    listProvider.length > 0 && listProvider.map((provider) => (
+                      <option key={provider._id} value={provider._id} >{provider.providerName}</option>
+                    ))
+                  }
                 </select>
               </div>
               <div className='i-o'>
                 <div className='i-name'>Mã phiếu</div>
-                <select name="idslip" id="idslip">
-                </select>
+                <input name="idslip" id="idslip" style={{ width: "100%", height: "40px", paddingLeft: "10px" }} value={newImportSlip.importSlipCode} readOnly />
               </div>
             </div>
             <div className='i-line2'>
               <div className='i-o'>
                 <div className='i-name'>Mã nguồn</div>
-                <select name="idsource" id="idsource">
-                </select>
+                <input style={{ width: "100%", height: "40px", paddingLeft: "10px" }} name="providerCode" id="idsource" value={providerInfor.providerCode} readOnly />
               </div>
               <div className='i-o'></div>
             </div>
             <div className='i-line3'>
               <div className='i-o'>
-              <div className='i-name'>Số điện thoại</div> <input type="text" />
+                <div className='i-name'>Số điện thoại</div> <input type="text" value={providerInfor.providerPhone} readOnly />
               </div>
               <div className='i-o'></div>
             </div>
             <div className='i-line4'>
               <div className='i-o'>
-                <div className='i-name'>Địa chỉ</div><textarea name="s-address" id="s-address" cols={50} rows={3}></textarea>
+                <div className='i-name'>Địa chỉ</div><textarea name="s-address" id="s-address" cols={50} rows={3} value={providerInfor.providerAddress} readOnly></textarea>
               </div>
               <div className='i-o'>
-                <div className='i-name'>Lý do nhập</div><textarea name="reasons" id="reasons" cols={50} rows={3}></textarea>
+                <div className='i-name'>Lý do nhập</div><textarea name="reason" value={newImportSlip.reason} onChange={(e) => setNewImportSlip({ ...newImportSlip, reason: e.target.value })} id="reasons" cols={50} rows={3}></textarea>
+              </div>
+            </div>
+          </div>
+          <div className='cis-table'>
+            <table className='cis-data'>
+              <tbody>
+                <tr>
+                  <th>STT</th>
+                  <th>Tên hàng hoá </th>
+                  <th>Mã hàng </th>
+                  <th>Đơn vị tính </th>
+                  <th>Đơn giá </th>
+                  <th>Số lượng </th>
+                  <th>Chiết khấu </th>
+                  <th>Thành tiền </th>
+                  <th>Xoá </th>
+                </tr>
+                {
+                  selectedProducts.length > 0 && selectedProducts.map((product, index) => (
+                    <tr className='list-item-product' key={product._id}>
+                      <td>{index + 1}</td>
+                      <td>{product.productName}</td>
+                      <td>{product.productCode}</td>
+                      <td>{product.productDVT}</td>
+                      <td>{formatCurrency(product.productPrice)}</td>
+                      <td>
+                        <input
+                          type='number'
+                          name='quantity'
+                          onChange={(e) => handleChangeQuantity(e, product._id)}
+                          style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", border: "none" }}
+                          placeholder='nhập số lượng'
+                        />
+                      </td>
+                      <td>
+                        <input
+                          type='text'
+                          name='discount'
+                          onChange={(e) => handleChangeDiscount(e, product._id)}
+                          style={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", border: "none" }}
+                          placeholder='nhập % chiết khấu'
+                        />
+                      </td>
+                      <td>{formatCurrency(calculateLineTotal(product))}</td>
+                      <td><i className="fa-solid fa-trash"></i> </td>
+                    </tr>
+                  ))
+                }
+                <tr>
+                  <td colSpan={7}>Tổng</td>
+                  <td colSpan={2} >{calculateTotalPrice() ? formatCurrency(calculateTotalPrice()) : formatCurrency(0)}</td>
+                </tr>
+              </tbody>
+
+            </table>
+          </div>
+
+          <div className='cis-contract'>
+            <div className='c-title'>
+              <div>
+                <i className="fa-solid fa-file-contract"></i>
+              </div>
+              <div>Hợp đồng</div>
+            </div>
+            <div className='c-input'>
+              <div className='cip'>
+                <div className='a'>
+                  Nội dung
+                </div>
+                <div className='b'>
+                  <input type="text" onChange={(e) => setContract({ ...contract, contractContent: e.target.value })} />
+                </div>
+              </div>
+              <div className='cip'>
+                <div className='a'>Hình ảnh </div>
+                <div className='b'>
+                  <input type="url" value={fileNames.join(', ')} onChange={(e) => handleChangeFileNameContract(e)} />
+                  <div className='cicon'>
+                    <label>
+                      <input type="file" accept='image/*' style={{ display: "none" }} multiple onChange={handleFileChange} />
+                      <i className="fa-solid fa-cloud-arrow-up"></i>
+                    </label>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className='cis-table'>
-            
-              <div className='th'>STT</div>
-              <div className='th'>Tên hàng hoá </div>
-              <div className='th'>Mã hàng </div>
-              <div className='th'>Đơn vị tính </div>
-              <div className='th'>Đơn giá </div>
-              <div className='th'>Số lượng </div>
-              <div className='th'>Chiết khấu </div>
-              <div className='th'>Thành tiền </div>
-              <div className='th'>Xoá </div>
-                    
-              <div className='td'>1</div>
-              <div className='td'>ĐT Samsung Galaxy Z </div>
-              <div className='td'>XXXXX </div>
-              <div className='td'>Cái </div>
-              <div className='td'>30.000.000 </div>
-              <div className='td'>10 </div>
-              <div className='td'>5% </div>
-              <div className='td'>285.000.000 </div>
-              <div className='td'><i className="fa-solid fa-trash"></i> </div>
-            
-              <div className='td'>2</div>
-              <div className='td'>ĐT Samsung Galaxy Z </div>
-              <div className='td'>XXXXX </div>
-              <div className='td'>Cái </div>
-              <div className='td'>30.000.000 </div>
-              <div className='td'>10 </div>
-              <div className='td'>5% </div>
-              <div className='td'>285.000.000 </div>
-              <div className='td'><i className="fa-solid fa-trash"></i> </div>
-            
-            
-              <div className='total'>Tổng</div>
-              <div className='total-amount'>324.980.000</div>
-            
-          </div>
-
-          <div className='cis-contract'>
-            <div className='c-title'><div><i className="fa-solid fa-file-contract"></i></div><div>Hợp đồng</div> </div>
-            <div className='c-input'>
-              <div className='cip'><div className='a'>Nội dung</div> <div className='b'><input type="text" /></div></div>
-              <div className='cip'><div className='a'>Hình ảnh </div><div className='b'><input type="url" /> <div className='cicon'><i className="fa-solid fa-cloud-arrow-up"></i></div></div></div>
-            </div>
-          </div>
-
-          <div className='cis-contract'>
+          {/* <div className='cis-contract'>
             <div className='c-title'><div><i className="fa-solid fa-expand"></i></div><div>Sở cứ</div></div>
             <div className='c-input'>
               <div className='cip'><div className='a'>Nội dung</div> <div className='b'><input type="text" /></div></div>
               <div className='cip'><div className='a'>Hình ảnh </div><div className='b'><input type="url" /> <div className='cicon'><i className="fa-solid fa-cloud-arrow-up"></i></div></div></div>
             </div>
-          </div>
+          </div> */}
 
-          <div className='cis-button'><button className='cis-cancel'>Huỷ</button><button className='cis-save'>Lưu</button></div>
+          <div className='cis-button'>
+            <button className='cis-cancel'>Huỷ</button>
+            <button className='cis-save' type='submit' onClick={handleSubmit}>Lưu</button>
+          </div>
         </div>
       </div>
 
@@ -133,7 +318,7 @@ const CreatedImportSlip = () => {
             initial={{ opacity: 0, scal: 0.5 }}
             transition={{ duration: 0.3 }}
           >
-            <UploadProductFromLocal onCancel={ handleCancelUploadLocal} />
+            <UploadProductFromLocal onCancel={handleCancelUploadLocal} selectedProducts={selectedProducts} setSelectedProducts={setSelectedProducts} isRefresh={isRefresh} setIsRefresh={setIsRefresh} />
           </motion.div>
         </div>
       )}
